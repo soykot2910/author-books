e036292015ac1de24d1cbf6abdae5f0ba757599d
soykot2910 <md.soykot2910@gmail.com>
2022-03-26 09:39:23 +0600

    suspended fallback add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React,{lazy} from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/common/Sidebar";
 import { FavouriteProvider } from "./AuthorContext";
+import { Spinner } from "./components/common/SVGIcons";
 
-const Authors =lazy(()=>import("./pages/Authors"))
-const FavouriteAuthors =lazy(()=>import("./pages/FavouriteAuthors"))
-
+const Authors = lazy(() => import("./pages/Authors"));
+const FavouriteAuthors = lazy(() => import("./pages/FavouriteAuthors"));
 
 const App = () => {
-
   return (
     <FavouriteProvider>
       <BrowserRouter>
         <div className="container">
           <Sidebar />
           <main className="main-content">
-            <Routes>
-              <Route path="/" element={<div className="no-data"><h1>Author Books</h1></div>} />
-              <Route path="/authors" element={<Authors />} />
-              <Route path="/favourite-authors" element={<FavouriteAuthors />} />
-              <Route path="*" element={<div className="no-data"><h1>Page Not Found</h1></div>} />
-            </Routes>
+            <Suspense fallback={<Spinner />}>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <div className="no-data">
+                      <h1>Author Books</h1>
+                    </div>
+                  }
+                />
+                <Route path="/authors" element={<Authors />} />
+                <Route
+                  path="/favourite-authors"
+                  element={<FavouriteAuthors />}
+                />
+                <Route
+                  path="*"
+                  element={
+                    <div className="no-data">
+                      <h1>Page Not Found</h1>
+                    </div>
+                  }
+                />
+              </Routes>
+            </Suspense>
           </main>
         </div>
       </BrowserRouter>
